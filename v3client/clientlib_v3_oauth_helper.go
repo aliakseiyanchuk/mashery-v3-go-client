f1d62b0ae6fa855542605c55b2a1cbad8bbde36b
lspwd2 <aliaksei@internal.yanchuk.nl>
2022-01-22 21:11:27 +0100

    Error reporting formatting.

--- v3client/clientlib_v3_oauth_helper.go
+++ v3client/clientlib_v3_oauth_helper.go
@@ -3,6 +3,7 @@ package v3client
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/aliakseiyanchuk/mashery-v3-go-client/masherytypes"
 	"github.com/aliakseiyanchuk/mashery-v3-go-client/transport"
 	"io/ioutil"
@@ -73,16 +74,18 @@ func (lcp *V3OAuthHelper) postForToken(data url.Values, creds *MasheryV3Credenti
 	defer req.Body.Close()
 	resp, err := lcp.client.Do(req)
 	if err != nil {
-		return nil, errors.New("Failed to retrieve response from the server")
-	}
-	if resp.StatusCode != 200 {
-		return nil, errors.New("Server returned unexpected error code")
+		return nil, errors.New(fmt.Sprintf("server call was rejected (%s)", err.Error()))
 	}
 
 	defer resp.Body.Close()
 	bodyText, err := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(fmt.Sprintf("server returned unexpected error code %d with message %s", resp.StatusCode, bodyText))
+	}
+
 	if err != nil {
-		return nil, errors.New("Failed to read response body")
+		return nil, errors.New(fmt.Sprintf("failed to read response body (%s)", err.Error()))
 	}
 
 	procResp := masherytypes.TimedAccessTokenResponse{
@@ -91,7 +94,7 @@ func (lcp *V3OAuthHelper) postForToken(data url.Values, creds *MasheryV3Credenti
 	}
 	err = json.Unmarshal(bodyText, &procResp)
 	if err != nil {
-		return nil, errors.New("Could not unmarshal access token response")
+		return nil, errors.New(fmt.Sprintf("could not unmarshal access token response (%s)", err.Error()))
 	}
 
 	procResp.ServerTime = ResponseDate(resp)
